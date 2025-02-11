71b7821d509fda463a4f3d9fc8dbfd8e01c028e3
ArchisDhakne <archisdhakne@gmail.com>
2025-02-11 20:37:56 +0530

    Forced Git to track client folder

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Description from "../components/Description"
+import GenerateBUtton from "../components/GenerateBUtton"
+import Header from "../components/Header"
+import Steps from "../components/Steps"
+import Testimonials from "../components/Testimonials"
+
+const Home = () => {
+  return (
+
+
+    <div>
+        <Header/>
+        <Steps/>
+        <Description/>
+        <Testimonials/>
+        <GenerateBUtton/>
+    </div>
+ 
+
+)
+}
+
+export default Home
