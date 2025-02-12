1eef04749a7405fe0cccf55233255a7decad0905
ArchisDhakne <archisdhakne@gmail.com>
2025-02-12 10:44:59 +0530

    emoved clinet serving form server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import connectDB from './config/mongodb.js';
 import userRoute from './routes/useRoute.js';
 import imageRouter from './routes/imageRoute.js';
@@ -15,16 +13,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use('/api/user', userRoute);
 app.use('/api/image', imageRouter);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
